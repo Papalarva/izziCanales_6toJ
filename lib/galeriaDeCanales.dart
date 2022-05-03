d3b0dc07b12b810b576803712dfb086a1d6b63b9
FlutLab.io <>
2022-05-03 16:19:02 +0000

    Autosave

--- lib/galeriaDeCanales.dart
+++ lib/galeriaDeCanales.dart
@@ -28,7 +28,7 @@ class _PaginaInicialState extends State<PaginaInicial> {
           itemBuilder: (BuildContext context, int index) {
             return Image.asset(
               images[index],
-              fit: BoxFit.cover,
+              fit: BoxFit.fitHeight,
             );
           },
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 20, crossAxisSpacing: 20),
